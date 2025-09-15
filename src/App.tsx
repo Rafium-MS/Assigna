429dc4d239fd139aaa97b310d33ff8851f3b2600
Rafium-MS <silveriorafaelsantos@gmail.com>
2025-09-15 14:38:46 -0300

    Fix input ref warning and expose service worker assets

--- src/App.tsx
+++ src/App.tsx
@@ -160,9 +160,16 @@ function useStore() {
   </div>
  );
 
- const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (props) => (
-  <input {...props} className={`w-full rounded-xl border px-3 py-2 outline-none focus:ring-2 focus:ring-black/20 dark:focus:ring-white/20 bg-white dark:bg-neutral-900 ${props.className||''}`} />
- );
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+  (props, ref) => (
+    <input
+      ref={ref}
+      {...props}
+      className={`w-full rounded-xl border px-3 py-2 outline-none focus:ring-2 focus:ring-black/20 dark:focus:ring-white/20 bg-white dark:bg-neutral-900 ${props.className || ''}`}
+    />
+  )
+);
+Input.displayName = 'Input';
  const Select: React.FC<React.SelectHTMLAttributes<HTMLSelectElement>> = (props) => (
   <select {...props} className={`w-full rounded-xl border px-3 py-2 bg-white dark:bg-neutral-900 ${props.className||''}`} />
  );
