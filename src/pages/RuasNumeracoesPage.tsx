b1f08ab04d1a517f52d44c0487e00038b6e1ee24
Rafium-MS <silveriorafaelsantos@gmail.com>
2025-09-15 14:07:15 -0300

    feat: add image annotator component

--- src/pages/RuasNumeracoesPage.tsx
+++ src/pages/RuasNumeracoesPage.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import ImageAnnotator from '../components/ImageAnnotator';
 
 export default function RuasNumeracoesPage(): JSX.Element {
-  return <div>Ruas</div>;
+  const handleAddStreet = (point: { x: number; y: number }) => {
+    console.log('Street', point);
+  };
+
+  const handleAddAddress = (point: { x: number; y: number }) => {
+    console.log('Address', point);
+  };
+
+  return (
+    <div className="p-4">
+      <ImageAnnotator
+        imageUrl="https://via.placeholder.com/800x600"
+        onAddStreet={handleAddStreet}
+        onAddAddress={handleAddAddress}
+      />
+    </div>
+  );
 }
+
